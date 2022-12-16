18b2510c3d7f747c8e1ba6d53563c453304b6fa0
Sanchitha Udana Wijesundara <udanasanchitha4@gmail.com>
2022-12-16 20:01:21 +0530

    second commit

--- src/Object/personalData.java
+++ src/Object/personalData.java
@@ -2,8 +2,18 @@ package Object;
 
 public class personalData {
 
+    // in the class you firstly create methods and you can develop towards
+
+    public personalData() {   // default constructor
+    }
+
+
+
     public void name(){
         System.out.println("Sanchitha");
     }
+
+
+
 
 }
