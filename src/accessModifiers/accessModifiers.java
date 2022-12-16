184546428e153f8ea2dd92cf58504a871c5a34bd
Sanchitha Udana Wijesundara <udanasanchitha4@gmail.com>
2022-12-16 19:02:53 +0530

    second commit

--- src/accessModifiers/accessModifiers.java
+++ src/accessModifiers/accessModifiers.java
@@ -1,4 +1,21 @@
 package accessModifiers;
 
 public class accessModifiers {
+
+    // default
+    // using in same package
+    int a;
+    int b;
+
+    // private
+    // using in relevant java class only
+    private String name;
+    private String password;
+
+    // public
+    // using in any where
+    public int summer;
+    public String som;
+
+
 }
